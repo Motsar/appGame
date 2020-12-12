5fb0dbafa2a48a5c005074e2930cc848f7a3da1a
martin.motsar <martin.motsar@khk.ee>
2020-12-12 20:02:08 +0200

    node app

--- app/server.js
+++ app/server.js
@@ -69,6 +69,6 @@ app.get('/get-profile', function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+app.listen(3000, '0.0.0.0' ,function () {
   console.log("app listening on port 3000!");
 });
